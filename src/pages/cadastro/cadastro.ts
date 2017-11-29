eb2ca9da9b2c559f519ae88aca6002d5002d18a0
Hugo Rosa <hugo.rosa2304@gmail.com>
2017-11-29 16:07:23 -0300

    Tela de cadastro

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -17,12 +17,12 @@ export class CadastroPage {
               public navCtrl: NavController, 
               public navParams: NavParams) {
 
-    let emailRegex = ("/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"); /*Expressão regular para emails*/
+    //let emailRegex = ("/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"); /*Expressão regular para emails*/
 
     this.cadastroForm = this.formBuilder.group({
       nome: ['', [Validators.required, Validators.minLength(3)]],
       usuario: ['', [Validators.required, Validators.minLength(6)]],
-      /*email: ['', Validators.compose, ([Validators.required, Validators.pattern(emailRegex)])],*/
+      email: ['', [Validators.required, Validators.minLength(22), Validators.maxLength(100)]],
       senha: ['', [Validators.required, Validators.minLength(8)]],
       confirmarSenha: ['', [Validators.required, Validators.minLength(8)]],
     });
